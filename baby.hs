0525f475695314a683f7c435effb6b6de9dc4b20
Erik Öskog <erik.oskog@gmail.com>
2015-11-08 21:38:13 +0100

    Before modified bmiTell

--- baby.hs
+++ baby.hs
@@ -57,11 +57,23 @@ capital :: String -> String
 capital [] = "Ooops, a empty string!"
 capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
 
-bmiTell :: (RealFloat a) => a -> String
-bmiTell bmi
-    | bmi <= 18.5 = "You're underweight, you emo, you!"
-    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
-    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height 
+    | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
+    | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"
     | otherwise = "You're a whale, congrats!"
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+    | a > b     = a
+    | otherwise = b
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a > b     = GT
+    | a == b    = EQ
+    | a < b     = LT
+
 
 
