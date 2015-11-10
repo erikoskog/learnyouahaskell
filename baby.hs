0159197eafd3edaa3de4a2d0472939d457978c34
Erik Öskog <erik.oskog@gmail.com>
2015-11-10 21:25:17 +0100

    Updates version of maximum'

--- baby.hs
+++ baby.hs
@@ -88,9 +88,17 @@ calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
 maximum' :: (Ord a) => [a] -> a
 maximum' [] = error "maximum of empty list"
 maximum' [x] = x
-maximum' (x:xs)
-        | x > maxTail = x
-        | otherwise = maxTail
-        where maxTail = maximum' xs
+maximum' (x:xs) = max x (maximum' xs)
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0    = []
+    | otherwise = x:replicate' (n-1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> a
+take' n _
+    | n <= 0    = []
+take' _ []      = []
+take' n (x:xs)  = x : take' (n-1) xs
 
 
