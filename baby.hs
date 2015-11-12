3172c9646673ef49fdd7803ba0cb75e817e731d5
Erik Öskog <erik.oskog@gmail.com>
2015-11-12 10:17:00 +0100

    Review the take' functon

--- baby.hs
+++ baby.hs
@@ -95,10 +95,14 @@ replicate' n x
     | n <= 0    = []
     | otherwise = x:replicate' (n-1) x
 
-take' :: (Num i, Ord i) => i -> [a] -> a
-take' n _
-    | n <= 0    = []
-take' _ []      = []
-take' n (x:xs)  = x : take' (n-1) xs
+-- take' :: (Num i, Ord i) => i -> [a] -> a
+-- take' n _
+--    | n <= 0    = []
+-- take' _ []      = []
+-- take' n (x:xs)  = x : take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
 
 
